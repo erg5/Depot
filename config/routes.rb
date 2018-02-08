8e274582d4e0f2117a8d6fde8da1ba01d364fb01
zxn1415 <zxn1415@gmail.com>
2018-02-07 21:53:19 -0500

    week 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
+  resources :products
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -6,5 +9,5 @@ Rails.application.routes.draw do
   get 'static_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  root 'store#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131195331) do
+ActiveRecord::Schema.define(version: 20180207201916) do
 
-  create_table "quotes", force: :cascade do |t|
-    t.string "author"
-    t.string "quotation"
-    t.string "source"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "Name"
-    t.string "Email"
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "image_url"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
