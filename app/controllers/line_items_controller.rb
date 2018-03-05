77936ec5eab2b2bf34c6aaab90348a02ad53ec39
zxn1415 <zxn1415@gmail.com>
2018-03-05 15:36:28 -0500

    lab 9

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   
-  include CurrentCart
+   include CurrentCart
   before_action :set_cart, only: [:create]
   
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
@@ -29,11 +29,11 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-     @line_item = @cart.add_product(product)
-
+  # @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
     respond_to do |format|
-format.html { redirect_to @line_item.cart }
-        # format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+      if @line_item.save
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -73,7 +73,13 @@ format.html { redirect_to @line_item.cart }
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
+    #def line_item_params
+    #  params.require(:line_item).permit(:product_id, :cart_id)
+    #end
+
     def line_item_params
     #  params.require(:line_item).permit(:product_id, :cart_id)
     params.require(:line_item).permit(:product_id)
     end
+
+end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,7 @@
 class LineItem < ApplicationRecord
-  belongs_to :product
+  belongs_to :order, optional: true
+  belongs_to :product, optional: true
+
   belongs_to :cart
 
   def total_price
@@ -7,3 +9,4 @@ class LineItem < ApplicationRecord
   end
 
 end
+
