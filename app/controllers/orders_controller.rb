219b0dea651929c88e15bced9e42f5141120a053
zxn1415 <zxn1415@gmail.com>
2018-03-07 23:39:47 -0500

    lab 10

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,9 @@
 class OrdersController < ApplicationController
-
+skip_before_action :authorize, only: [:new, :create]
 include CurrentCart
 before_action :set_cart, only: [:new, :create]
-# key new method needed to the ensure_cart_isnt_empty 
-before_action :ensure_cart_isnt_empty, only: :new
-before_action :set_order, only: [:show, :edit, :update, :destroy]
-# before_action :set_order, only: [:show, :edit, :update, :destroy]
+  
+  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /orders
   # GET /orders.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+ 
+  get 'admin/index'
+  
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+    
+  #get 'sessions/new'
+
+  #get 'sessions/create'
+
+  #get 'sessions/destroy'
+
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
@@ -12,5 +28,7 @@ Rails.application.routes.draw do
   get 'static_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'store#index'
+
 end
